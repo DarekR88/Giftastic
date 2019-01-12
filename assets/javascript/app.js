52d870229ef5778eb9a5f6592b822201eac615eb
DarekR88 <dradmclad@gmail.com>
2019-01-11 23:15:21 -0600

    bous working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,6 +27,9 @@ $(".submitButton").click(function(event){
 });
 
 makeButtons();
+
+// variable for holding the limit of the query
+var limit = 10; 
 // variable for storing "this"
 var toon;
 // variable that represents the giphy query search and images
@@ -41,8 +44,10 @@ var playingGif = false;
 $(document).on('click','.gifButton',function(){
    $("#gifs").empty();
    $("#gifsTwo").empty();
+   limit = 10;
    toon = $(this).attr("toonName");
-   var giphyUrl = "https://api.giphy.com/v1/gifs/search?q=" + toon + "&api_key=fk15UAOOrgq9oDZfXGgEc9XPVbDuh715&limit=10";
+   $(".addGifs").attr("toonName", toon)
+   var giphyUrl = "https://api.giphy.com/v1/gifs/search?q=" + toon + "&api_key=fk15UAOOrgq9oDZfXGgEc9XPVbDuh715&limit=" + limit + "";
    console.log(toon);
    $.ajax({
     url: giphyUrl,
@@ -59,15 +64,44 @@ $(document).on('click','.gifButton',function(){
       gifPlaying = response.data[i].images.original.url
       // add the still images to the html
       if (i <= 4){
-        $("#gifs").append("<figure><img class='stillGif' id='playGif' src= " + still + " animated=" + gifPlaying + " stillImage=" + still + " imageState='notPlaying' height='200' width='200'><figcaption>Rating: " + rating + "</figcaption></figure>");
+        $("#gifs").append("<figure><img class='stillGif' id='playGif' src= " + still + " animated=" + gifPlaying + " stillImage=" + still + " imageState='notPlaying' height='200' width='300'><figcaption>Rating: " + rating + "</figcaption></figure>");
       } else {
-        $("#gifsTwo").append("<figure><img class='stillGif' id='playGif' src= " + still + " animated=" + gifPlaying + " stillImage=" + still + " imageState='notPlaying' height='200' width='200'><figcaption>Rating: " + rating + "</figcaption></figure>");
-      }
+        $("#gifsTwo").append("<figure><img class='stillGif' id='playGif' src= " + still + " animated=" + gifPlaying + " stillImage=" + still + " imageState='notPlaying' height='200' width='300'><figcaption>Rating: " + rating + "</figcaption></figure>");
+      };
+    };     
+  });
+});
 
-      // gifStill = $("#gifs").append("<img class='stillGif' id= 'playGif" + i + "' src= " + still + " height='200' width='200'><p>Rating: " + rating + "</p>");
-      
-    };  
-      $(".stillGif").click(function(){
+// function for adding more gifs to the page 
+      $(".addGifs").click(function(){
+          limit +=10
+          console.log(limit)
+          toon = $(this).attr("toonName");
+          var giphyUrl = "https://api.giphy.com/v1/gifs/search?q=" + toon + "&api_key=fk15UAOOrgq9oDZfXGgEc9XPVbDuh715&limit=" + limit + "";
+          console.log(toon);
+          $.ajax({
+           url: giphyUrl,
+           method: "GET"
+         }).then(function(response){
+           console.log(response)
+           for (var i = 0; i < response.data.length; i++){
+            // create a variable that will hold the url of the desired image
+            still = response.data[i].images.downsized_still.url;
+            // variable that will hold the ratings 
+            rating = response.data[i].rating
+            // variable that holds the url of animated gif
+            gifPlaying = response.data[i].images.original.url
+            // add the still images to the html
+            if (i > (limit -11) && i < (limit -5)){
+              $("#gifs").append("<figure><img class='stillGif' id='playGif' src= " + still + " animated=" + gifPlaying + " stillImage=" + still + " imageState='notPlaying' height='200' width='300'><figcaption>Rating: " + rating + "</figcaption></figure>");
+            } else if (i > (limit -6)) {
+              $("#gifsTwo").append("<figure><img class='stillGif' id='playGif' src= " + still + " animated=" + gifPlaying + " stillImage=" + still + " imageState='notPlaying' height='200' width='300'><figcaption>Rating: " + rating + "</figcaption></figure>");
+            }
+          }
+         })
+      })
+
+      $(document).on('click','.stillGif',function(){
         state = $(this).attr("imageState")
         imageSrc = $(this).attr("src");
         animeGifUrl = $(this).attr("animated");
@@ -80,16 +114,5 @@ $(document).on('click','.gifButton',function(){
           $(this).attr("imageState", "notPlaying");
         };
       });
-      
-      
-
-      
-    
-  });
-});
-
-
-
-
 
 
